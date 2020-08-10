3b5d0d2ce9e96591eca4d1c0010f38db2fa8bbb4
cander <cander_liubiao@126.com>
2020-08-10 10:58:23 +0800

    优化

--- groupmethod.go
+++ groupmethod.go
@@ -1,21 +1,8 @@
 package xmux
 
 import (
-	"log"
 	"net/http"
 )
-
-type MethodsRoute map[string]*Route
-
-func (mr MethodsRoute) getRoute(method string, handler func(http.ResponseWriter, *http.Request)) *Route {
-	if _, ok := mr[method]; ok {
-		log.Fatal("method post duplicate")
-	}
-	mr[method] = &Route{
-		handle: http.HandlerFunc(handler),
-	}
-	return mr[method]
-}
 
 func (gr *GroupRoute) Post(pattern string, handler func(http.ResponseWriter, *http.Request)) *Route {
 	if pt, ok := gr.makeRoute(pattern); ok {
---
+++ mr.go
@@ -0,0 +1,18 @@
+package xmux
+
+import (
+	"log"
+	"net/http"
+)
+
+type MethodsRoute map[string]*Route
+
+func (mr MethodsRoute) getRoute(method string, handler func(http.ResponseWriter, *http.Request)) *Route {
+	if _, ok := mr[method]; ok {
+		log.Fatal("method post duplicate")
+	}
+	mr[method] = &Route{
+		handle: http.HandlerFunc(handler),
+	}
+	return mr[method]
+}
