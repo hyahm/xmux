9ff62b575dbee108f6f7e30de23cbb5355826243
cander <cander_liubiao@126.com>
2021-04-26 14:40:08 +0800

    增加 stringtobytes and bytestostring

--- helper.go
+++ helper.go
@@ -3,6 +3,7 @@ package xmux
 import (
 	"reflect"
 	"runtime"
+	"unsafe"
 )
 
 func compareFunc(f1 interface{}, f2 interface{}) bool {
@@ -20,4 +21,14 @@ func compareFunc(f1 interface{}, f2 interface{}) bool {
 	name1 := runtime.FuncForPC(tm1).Name()
 	name2 := runtime.FuncForPC(tm2).Name()
 	return name1 == name2
+}
+
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+func StringToBytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
 }
