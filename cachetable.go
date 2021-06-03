182268294905931adf1c64d66f925a12c832972f
cander <cander_liubiao@126.com>
2021-06-03 11:16:05 +0800

    代码优化

--- cachetable.go
+++ cachetable.go
@@ -7,14 +7,14 @@ type cacheTable struct {
 	mu    *sync.RWMutex
 }
 
-var ctLocker *cacheTable
+// var ctLocker *cacheTable
 
-func init() {
-	ctLocker = &cacheTable{
-		cache: make(map[string]*rt),
-		mu:    &sync.RWMutex{},
-	}
-}
+// func init() {
+// 	ctLocker = &cacheTable{
+// 		cache: make(map[string]*rt),
+// 		mu:    &sync.RWMutex{},
+// 	}
+// }
 
 func (ct cacheTable) Get(key string) (*rt, bool) {
 	ct.mu.RLock()
