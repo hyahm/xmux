672119000a183bfc0e6aba0302476dc07b1597f8
cander <cander_liubiao@126.com>
2022-04-28 16:12:40 +0800

    rm cache

--- cache.go
+++ cache.go
@@ -4,6 +4,15 @@ import "github.com/hyahm/xmux/cache"
 
 const CacheKey = "CacheKey"
 
+type Cacher[K string, V []byte] interface {
+	Add(key K, value V) (K, bool) // 添加值， 如果返回 k, true 说明有删除值，并返回删除的key
+	Remove(key K)                 // 移除k
+	Len() int                     // 长度
+	OrderPrint(int)               // 顺序打印
+	Get(key K) (V, bool)          // 获取值
+	LastKey() K                   // 获取最先要删除的key
+}
+
 func ResponseCache() *RouteGroup {
 	cache.InitResponseCache()
 	cache := NewRouteGroup()
