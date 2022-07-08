0ddc193c2ef787b0220d78cee72e69433f7e0a79
cander <cander_liubiao@126.com>
2022-07-08 18:07:07 +0800

    添加rsa加密解密

--- tls.go
+++ tls.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"encoding/pem"
 	"io/ioutil"
 	"log"
@@ -119,4 +120,151 @@ func createCertificateFile(name string, cert *x509.Certificate, key *rsa.Private
 
 	ioutil.WriteFile(priv_f, priv_b64, 0744)
 
+}
+
+func GenRsa(prikey, pubkey, crtkey string) error {
+	// openssl genrsa -out private.pem 2048
+	// openssl rsa -in private.pem -pubout -out public.pem
+	// openssl pkeyutl -encrypt -inkey public.pem  -pubin -in f.txt -out fe.txt
+	// openssl pkeyutl -decrypt -inkey private.pem -in fe.txt -out ffff.txt
+	pri, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	derText := x509.MarshalPKCS1PrivateKey(pri)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derText,
+	}
+	f, err := os.OpenFile(prikey, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	pem.Encode(f, block)
+
+	// 生成私钥
+
+	ptext, err := x509.MarshalPKIXPublicKey(&pri.PublicKey)
+	if err != nil {
+		return err
+	}
+	pblock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: ptext,
+	}
+
+	f, err = os.OpenFile(pubkey, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	pem.Encode(f, pblock)
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject:      pkix.Name{CommonName: "xmux"},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		KeyUsage:     x509.KeyUsageCertSign | x509.KeyUsageCRLSign | x509.KeyUsageDigitalSignature,
+	}
+	if ip := net.ParseIP("localhost"); ip != nil {
+		template.IPAddresses = append(template.IPAddresses, ip)
+	} else {
+		template.DNSNames = append(template.DNSNames, "localhost")
+	}
+	cert, err := x509.CreateCertificate(rand.Reader, &template, &template, &pri.PublicKey, pri)
+	if err != nil {
+		return err
+	}
+	cblock := &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: cert,
+	}
+	f, err = os.OpenFile(crtkey, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return pem.Encode(f, cblock)
+}
+
+func RsaDecryptFromBase64(s string, priviteKeyPath string) ([]byte, error) {
+	b, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.Open(priviteKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	//获取文件内容
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	//对密文进行解密
+	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, b)
+}
+
+func RsaDecryptFromString(s string, priviteKeyPath string) ([]byte, error) {
+	b := []byte(s)
+	file, err := os.Open(priviteKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	//获取文件内容
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	//对密文进行解密
+	return rsa.DecryptPKCS1v15(rand.Reader, privateKey, b)
+}
+
+//RSA加密
+func RSA_Encrypt(plainText []byte, path string) ([]byte, error) {
+	//打开文件
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	//读取文件的内容
+	info, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	//x509解码
+
+	publicKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	//类型断言
+	publicKey := publicKeyInterface.(*rsa.PublicKey)
+	//对明文进行加密
+	return rsa.EncryptPKCS1v15(rand.Reader, publicKey, plainText)
 }
---
+++ tls_test.go
@@ -0,0 +1,38 @@
+package xmux
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"os"
+	"testing"
+)
+
+func TestDecrypt(t *testing.T) {
+	s := "OCT+5EHncL1sZg0BRniPz5x3A0MTv24pltkjzyoYE3ucf46MtCZGNs+NojDD+6Rl7z1wvP6jJeAlmaUYZukuOfvf7sdcwA4KZWCZycc25RIavoFzNrPbjqPpsf0u2k/BCleP7Dc8d3Ca34ZQlrlIIGxwj13pBKFHSMrR8YZtgvn9EueXM4/ENpq2MY9eamm2X5e6IsmfFl8J+L33U4NNhokXCBo/5wcXfwFiIBzSJXf9R+NRWjCITVxgyctiWyx4jtu8NrKYaiNDUhuyrsf1jmzgPgAKEOeC7VuwV0HRqa9UgNsAV7P6K8ZBMsvrXoEMWB6HQRZ28aLhtevrbFeymA=="
+	b, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	file, err := os.Open("pri.pem")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	//获取文件内容
+	info, _ := file.Stat()
+	buf := make([]byte, info.Size())
+	file.Read(buf)
+	//pem解码
+	block, _ := pem.Decode(buf)
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	//对密文进行解密
+	plainText, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, b)
+	t.Log(string(plainText))
+
+}
